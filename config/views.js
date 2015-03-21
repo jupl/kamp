65cf1dc63aea29c8f3146cd250c020d0fdbc3f3e
jupl <juan.placencia.512@gmail.com>
2015-03-21 13:16:08 -0500

    Add note for swig helpers

--- config/views.js
+++ config/views.js
@@ -1,7 +1,11 @@
 /*
  * Define options for the view templating system in the application.
  */
+import swig from 'swig';
 import {development} from './environment';
 
 export default 'swig';
 export const cache = !development;
+
+// Define filters here
+// ex: swig.setFilter('...', function() { ... });
