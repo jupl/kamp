270e84c6c8c9309a2fb943ced4d517dabb6a06d9
jupl <juan.placencia.512@gmail.com>
2015-03-21 18:57:02 -0500

    Add modernizr and touch class support

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,11 +1,18 @@
+import 'browsernizr/test/touchevents';
+import modernizr from 'browsernizr';
 import domready from 'domready';
 import fastclick from 'fastclick';
 import document from 'global/document';
 
 domready(addFastClick);
+domready(checkIfTouch);
 
 function addFastClick() {
   if('addEventListener' in document) {
     fastclick.attach(document.body);
   }
+}
+
+function checkIfTouch() {
+  document.body.className = modernizr.touchevents ? 'touch' : 'no-touch';
 }
