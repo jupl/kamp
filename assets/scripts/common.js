c0f0f094888a003ac70efe1c0a52ad46682381d9
jupl <juan.placencia.512@gmail.com>
2015-03-21 21:58:41 -0500

    Remove touch class logic as it is handled by modernizr

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -5,14 +5,9 @@ import fastclick from 'fastclick';
 import document from 'global/document';
 
 domready(addFastClick);
-domready(checkIfTouch);
 
 function addFastClick() {
   if('addEventListener' in document) {
     fastclick.attach(document.body);
   }
-}
-
-function checkIfTouch() {
-  document.body.className = modernizr.touchevents ? 'touch' : 'no-touch';
 }
